61d96a81e5558c772de884ff9c07924588cbb0a9
David Bakare <bakaredavid007@gmail.com>
2023-03-05 18:49:45 +0100

    raycaster

--- main.c
+++ main.c
@@ -28,13 +28,122 @@ int map[] =
 {
     1, 1, 1, 1, 1, 1, 1, 1,
     1, 0, 1, 0, 0, 0, 0, 1,
-    1, 0, 1, 0, 0, 0, 0, 1,
-    1, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 1, 0, 1, 1, 0, 1,
+    1, 0, 1, 1, 1, 1, 0, 1,
     1, 0, 0, 0, 0, 0, 0, 1,
     1, 0, 0, 0, 0, 1, 0, 1,
     1, 0, 0, 0, 0, 0, 0, 1,
     1, 1, 1, 1, 1, 1, 1, 1,
 };
+
+void ray_casting(SDL_Renderer **renderer, float px, float py, float pa) {
+    int r, mx, my, mp, dof, side, wall_angle;
+    float vx, vy, rx, ry, ra, xo, yo, disV, disH;
+
+    ra = FixAng(pa + 30); // ray set back 30 degrees
+
+    for (r = 0; r < 60; r++) {
+        //---Vertical---
+        wall_angle = 0;
+        dof = 0;
+        side = 0;
+        disV = 100000;
+        float Tan = tan(degToRad(ra));
+        if (cos(degToRad(ra)) > 0.001) {
+            rx = (((int) px >> 6) << 6) + 64;
+            ry = (px - rx) * Tan + py;
+            xo = 64;
+            yo = -xo * Tan;
+        } else if (cos(degToRad(ra)) < -0.001) {
+            rx = (((int) px >> 6) << 6) - 0.0001;
+            ry = (px - rx) * Tan + py;
+            xo = -64;
+            yo = -xo * Tan;
+        } else {
+            rx = px;
+            ry = py;
+            dof = 8;
+        } // looking up or down. no hit
+
+        while (dof < 8) {
+            mx = (int) (rx) >> 6;
+            my = (int) (ry) >> 6;
+            mp = my * mapX + mx;
+            if (mp > 0 && mp < mapX * mapY && map[mp] == 1) {
+                dof = 8;
+                disV = cos(degToRad(ra)) * (rx - px) - sin(degToRad(ra)) * (ry - py); // hit
+            } else {
+                rx += xo;
+                ry += yo;
+                dof += 1;
+            } // check next horizontal
+        }
+        vx = rx;
+        vy = ry;
+
+        //---Horizontal---
+        dof = 0;
+        disH = 100000;
+        Tan = 1.0 / Tan;
+        if (sin(degToRad(ra)) > 0.001) {
+            ry = (((int) py >> 6) << 6) - 0.0001;
+            rx = (py - ry) * Tan + px;
+            yo = -64;
+            xo = -yo * Tan;
+        } else if (sin(degToRad(ra)) < -0.001) {
+            ry = (((int) py >> 6) << 6) + 64;
+            rx = (py - ry) * Tan + px;
+            yo = 64;
+            xo = -yo * Tan;
+        } else {
+            rx = px;
+            ry = py;
+            dof = 8;
+        } // looking straight left or right
+
+        while (dof < 8) {
+            mx = (int) (rx) >> 6;
+            my = (int) (ry) >> 6;
+            mp = my * mapX + mx;
+            if (mp > 0 && mp < mapX * mapY && map[mp] == 1) {
+                dof = 8;
+                disH = cos(degToRad(ra)) * (rx - px) - sin(degToRad(ra)) * (ry - py); // hit
+            } else {
+                rx += xo;
+                ry += yo;
+                dof += 1;
+            }
+        }
+        SDL_SetRenderDrawColor(*renderer, 195, 20, 0, SDL_ALPHA_OPAQUE);
+        if (disV < disH) {
+            rx = vx;
+            ry = vy;
+            disH = disV;
+            wall_angle = 1;
+            SDL_SetRenderDrawColor(*renderer, 253, 14, 0, SDL_ALPHA_OPAQUE); // horizontal hit first
+        }
+        SDL_RenderDrawLine(*renderer, px, py, rx, ry); // draw 2D ray
+
+        int ca = FixAng(pa - ra);
+        disH = disH * cos(degToRad(ca));
+        int lineH = (mapS * 320) / (disH);
+        if (lineH > 320) {
+            lineH = 320;
+        }
+        int lineOff = 160 - (lineH >> 1);
+
+        SDL_Rect wall = {r * 8 + 530, lineOff, 8, lineH};
+        if(wall_angle == 1)
+            SDL_SetRenderDrawColor(*renderer, 253, 14, 0, SDL_ALPHA_OPAQUE);
+        else
+            SDL_SetRenderDrawColor(*renderer, 195, 20, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderFillRect(*renderer, &wall); // draw vertical wall
+
+        ra = FixAng(ra - 1);
+    }
+}
+
+
 
 void DrawMap(SDL_Renderer **Renderer)
 {
@@ -119,8 +228,8 @@ int main(int argc, char *argv[])
     float px, py, pdx, pdy, pa;
 
     //line
-    px = WIDTH / 2;
-    py = HEIGHT / 2;
+    px = 80;
+    py = 80;
     pa = 0;
     pdx = cos(degToRad(pa));
     pdy = -sin(degToRad(pa));
@@ -204,6 +313,7 @@ int main(int argc, char *argv[])
         ClearBackground(&Renderer);
         DrawMap(&Renderer);
         DrawPlayer(&Renderer, px, py, pdx, pdy, pa);
+        ray_casting(&Renderer, px, py, pa);
         SDL_RenderPresent(Renderer);
     }
     closeProgram(&Window, &Renderer);
