1d91f6d51c394ef5d767970bd227b491e2f14c13
David Bakare <bakaredavid007@gmail.com>
2023-03-10 22:58:41 +0100

    added header file

---
+++ Version-1/main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <SDL.h>
+
+/* OPenGL headers */
+#include<OpenGL/glu.h>
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+
+
+#define WID 1024 /* window width */
+#define HEI  512 /* window height */
+#define XPOS (WID / 4)
+#define YPOS (HEI / 4)
+
+
+#endif
