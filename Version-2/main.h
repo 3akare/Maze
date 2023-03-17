08ce6fae7c5fd2c953dcaa9d4c030dff9c34d001
David Bakare <bakaredavid007@gmail.com>
2023-03-17 03:00:42 +0100

    raw code

---
+++ Version-2/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include<OpenGL/glu.h>
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+#include <math.h>
+
+
+#endif
---
+++ Version-3/main.h
@@ -0,0 +1,25 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <SDL.h>
+#include<OpenGL/glu.h>
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+#include <math.h>
+
+#include "Textures/All_Textures.ppm"
+#include "Textures/sky.ppm"
+#include "Textures/title.ppm"
+#include "Textures/won.ppm"
+#include "Textures/lost.ppm"
+#include "Textures/sprites.ppm"
+
+/* OpenGL Headers */
+#include <SDL_opengl.h>
+#include <glu.h>
+
+
+#endif
